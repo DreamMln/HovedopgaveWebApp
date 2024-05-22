7c9e33be66e198ba4d4465e5340bf94c0d5b4712
DreamMln <maix2374@edu.zealand.dk>
2024-05-22 23:10:12 +0200

    ny

--- borger.js
+++ borger.js
@@ -5,19 +5,21 @@ const borger = Vue.createApp({
     data() {
         return {
 			regiArray: [], //tomt array til at indeholde data omkring borgere
+			pauseArray: [], //tomt array til at indeholde data omkring borgere
 			//obj. in another obj.
 			addRegi:{
 				ind: "",
 				ud: "",
-				addPause:{
-					pauseStart: "",
-					pauseSlut: ""
-				}
+			},
+			addPause:{
+				pauseStart: "",
+				pauseSlut: ""
 			},
 			pauseArray: [],
             noteArray: [], //tomt array til at indeholde data omkring borgernoter
 			//json obj - skal hedde det samme
 			addNote:{
+				noteID: 0,
 				noteOmBorger: "",
 				datoTid: "",
 			},
@@ -29,7 +31,10 @@ const borger = Vue.createApp({
 			addMessage: "",
 			deleteMessage: "",
 			id: 0,
-			noteID: 0,
+			//lommeregner/udregn procent
+			num1: 0,
+			num2: 0,
+			udregn: 0,
 		}
     },
 
@@ -42,11 +47,11 @@ const borger = Vue.createApp({
 		let id = urlParameters.get('id')
 		console.log("ID:" + id)
 		//videresender værdien, sammen med baseurl
-        this.getRoutingBorger(baseUrl, id)
+		this.getRoutingBorger(baseUrl, id)
         //created, kalder dem, istedet for this.id,
 		this.getAllRegi(baseUrl, id)
 		this.getAllNoter(baseUrl, id)
-
+		this.getAllPauser(baseUrl, id)
 	},
 	methods: {
 		//Get borger by id
@@ -74,17 +79,30 @@ const borger = Vue.createApp({
 			alert("Fejl get all regi " + ex.message)
 			}
 		},
+		/////////BORGER PAUSER//////
+		async getAllPauser(url, id){
+			try{
+			//response with axios
+			//det er en borger, derfor id og pause sti, kig i controlleren
+			const response = await axios.get(url + "/" + id + "/BorgerPauser")
+			this.pauseArray = await response.data
+			}
+		catch(ex){
+			alert("Fejl get all regi " + ex.message)
+			}
+		},
+
 		async opretRegiBorger(url, id){
 			console.log(this.addRegi)
 			try{
 				const response = await axios.post(url + "/" + id + "/BorgerRegistreringer", this.addRegi)
 				//response = await axios.post(baseUrl, this.addRegi)
 				this.addRegi = response.status + " " + response.statusText
-				this.getAllRegi()
+				this.getAllRegi(url, id)
 				console.log(this.regiArray)
 			}
 			catch(ex){
-				{alert("Fejl i add borger " + ex.message)}
+				{alert("Fejl i add regi " + ex.message)}
                 // alert("Fejl! Der er noget galt " + response.status)
 			}
 		},
@@ -118,14 +136,38 @@ const borger = Vue.createApp({
 		async sletNote(id, noteID){
 			console.log(this.deleteNote)
 			try{
-				const response = await axios.delete(baseUrl + "/" + id + "/BorgerNoter" + "?noteID=" + noteID , this.deleteNote)
-				this.deleteNote = "Responskode: " + response.status + " " + response.statusText
-				this.getAllNoter(baseUrl, id)
-				console.log(this.noteArray)
+				const response = await axios.delete(`${baseUrl}/${id}/BorgerNoter?noteID=${noteID}` , this.deleteNote)
+				this.deleteNote = `Responskode: ${response.status} ${response.status}`
+				if(response.status === 200){
+					await this.getAllNoter(baseUrl, id)
+					console.log(this.noteArray)
+				}else{
+					console.error(`kunne ikke slette note: ${response.statusText}`)
+				}
 			}
 			catch(ex){
 				{alert("Fejl! Slet en note " + ex.message)}
 			}
+		},
+		//lommeregner
+		add() {
+			this.udregn = this.num1 + this.num2;
+		},
+		subtract() {
+			this.udregn = this.num1 - this.num2;
+		},
+		multiply() {
+			this.udregn = this.num1 * this.num2;
+		},
+		divide() {
+			if (this.num2 === 0) {
+			alert("Kan ikke dividere med nul!");
+			} else {
+			this.udregn = this.num1 / this.num2;
+			}
+		},
+		calculatePercentage() {
+			this.udregn = (this.num1 * this.num2) / 100;
 		},
 	}
 })
